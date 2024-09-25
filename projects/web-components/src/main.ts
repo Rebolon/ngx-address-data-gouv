cf7ce1f92675fadeb0f11c9a8feaeda2df32ac70
Rebolon <richard.tribes@gmail.com>
2024-09-25 17:22:23 +0200

    for web component: move from NgModule to bootstrapApplication clean tsconfigs

--- projects/web-components/src/main.ts
+++ projects/web-components/src/main.ts
@@ -1,12 +1,18 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { AddressSearchComponent } from "../../ngx-address-data-gouv-search/src/lib/ngx-address-data-gouv.component";
 
-import { StandaloneModule } from './app/standalone.module';
-import { environment } from './environments/environment';
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient()],
+};
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(StandaloneModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AddressSearchComponent, appConfig)
+  .then((app) => {
+    const elRiAd = createCustomElement(AddressSearchComponent, {
+      injector: (app).injector
+    });
+    customElements.define('ngx-address-data-gouv-search', elRiAd);
+  })
+  .catch((err) => console.error(err));
